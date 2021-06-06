b2d29d0ba6b8ca3801f70582165af214e2799c46
Ilya <arzaevilya@gmail.com>
2021-06-06 16:13:38 +0200

    second

---
+++ IncrementDecrement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IncrementDecrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine(a);
+        }
+    }
+}
