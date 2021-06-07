6b17507d46360846fe6930d873931eec640cb075
Ilya <arzaevilya@gmail.com>
2021-06-07 18:09:01 +0200

    HomeWorkWhileEven

---
+++ WhileOneHomeWork/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WhileOneHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Type limit number");
+            int limit = int.Parse(Console.ReadLine());
+            while(count < limit)
+            {
+                count++;
+                if (count % 2 == 0)
+                {
+                    Console.WriteLine(count + " is even");
+                }
+                else
+                {
+                    Console.WriteLine(count + " is not even");
+                }
+            }
+        }
+    }
+}
