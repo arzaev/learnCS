dee16a268e993f530766567a6f21eaa0f2df1c6e
Ilya <arzaevilya@gmail.com>
2021-06-07 17:11:55 +0200

    switch2

--- SwitchOne/Program.cs
+++ SwitchOne/Program.cs
@@ -6,7 +6,19 @@ namespace SwitchOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("You typed one");
+                    break;
+                case 2:
+                    Console.WriteLine("You typed two");
+                    break;
+                default:
+                    Console.WriteLine("Number is wrong");
+                    break;
+            }
         }
     }
 }
