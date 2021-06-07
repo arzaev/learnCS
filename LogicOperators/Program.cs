0b72689322170d4c093ca1df022cbb347c8f6717
Ilya <arzaevilya@gmail.com>
2021-06-07 16:14:54 +0200

    logic2

--- LogicOperators/Program.cs
+++ LogicOperators/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
+
+// && Сокр И
+// && || сокр или
+// И
+// | Или
+// ! Не
+
 namespace LogicOperators
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isHighTemperature = true;
+            bool hasNoCooling = true;
+
+            if (isHighTemperature || hasNoCooling)
+            {
+                Console.WriteLine("It's bad for processor");
+            }
         }
     }
 }
