0d69576e515aad6abfc8ceb3c0e2dd693fb7f192
Ilya <arzaevilya@gmail.com>
2021-06-07 21:52:08 +0200

    loopfor2

---
+++ LoopFor2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LoopFor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int limit = int.Parse(Console.ReadLine());
+            //    for (int i = 0; i < limit; i++)
+            //    {
+
+            //    }
+
+            for (;;)
+            {
+                Console.WriteLine("for is working");
+            }
+
+            Console.ReadLine();
+        }
+}
+}
