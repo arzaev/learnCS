337d5158ae074a2b07e6ddcb94f57ac4a2df6a55
Ilya <arzaevilya@gmail.com>
2021-06-07 18:34:52 +0200

    DoWhile

---
+++ DoWhile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 5;
+
+            do
+            {
+                count++;
+                Console.WriteLine(count);
+            } while (count < 5);
+  
+        }
+    }
+}
