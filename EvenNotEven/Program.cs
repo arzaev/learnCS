10f1d0aca535759923cab96a5123a898890b511c
Ilya <arzaevilya@gmail.com>
2021-06-07 21:32:55 +0200

    evenOddNumbers

---
+++ EvenNotEven/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EvenNotEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint oddNumbersCount = 0;
+            uint evenNumbersCount = 0;
+
+            int currentValue = int.Parse(Console.ReadLine());
+            int limit = int.Parse(Console.ReadLine());
+
+            while (currentValue <= limit)
+            {
+                if (currentValue % 2 == 0)
+                {
+                    evenNumbersCount++;
+                }
+                else
+                {
+                    oddNumbersCount++;
+                }
+                currentValue++;
+            }
+            Console.WriteLine("Odd: " + oddNumbersCount);
+            Console.WriteLine("Even: " + evenNumbersCount);
+            Console.WriteLine("Number of numbers: " + currentValue);
+        }
+    }
+}
