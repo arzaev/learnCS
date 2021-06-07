ef971a61db117317601bcb412fbe16ee1d806d60
Ilya <arzaevilya@gmail.com>
2021-06-07 22:14:08 +0200

    breakfor

---
+++ LoopBreak/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LoopBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string msg = Console.ReadLine();
+                if(msg == "exit")
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
