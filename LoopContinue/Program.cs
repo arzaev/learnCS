ec586aa34fbd795feb0cfcf1ce6c9676c58829b8
Ilya <arzaevilya@gmail.com>
2021-06-07 22:37:15 +0200

    continueLoop

---
+++ LoopContinue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LoopContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 2)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
