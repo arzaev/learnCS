9d7811f2d438a23cff276d3e153df43858665d67
Ilya <arzaevilya@gmail.com>
2021-06-08 15:41:38 +0200

    loopinside

---
+++ LoopInside/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LoopInside
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Loop 1 interation " + i);
+            //    for (int j = 0; j <= 3; j++)
+            //    {
+            //        Console.WriteLine("\tLoop 2 interation " + j);
+
+            //        for (int k = 0; k < 2; k++)
+            //        {
+            //            Console.WriteLine("\t\tLoop 2 interation " + k);
+            //        }
+            //    }
+            //}
+
+            Console.WriteLine("height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
