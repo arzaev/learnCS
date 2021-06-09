36ef6fb993968183bab54c3cf071d6d7758d5007
Ilya <arzaevilya@gmail.com>
2021-06-09 15:50:26 +0200

    realArray

---
+++ realArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace realArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 34, 54, 23, 11, 5, 234, 34, 2 };
+
+            int result = myArray.Where(i => i % 2 == 0).Sum();
+            Console.WriteLine(result);
+
+        }
+    }
+}
