4b53ec8bfefd954339455c3e64fae3435de05c1c
Ilya <arzaevilya@gmail.com>
2021-06-10 16:06:49 +0200

    2array

--- TwoDimensionalArray/Program.cs
+++ TwoDimensionalArray/Program.cs
@@ -7,9 +7,29 @@ namespace TwoDimensionalArray
         static void Main(string[] args)
         {
             int[,] myArray;
-            myArray = new int[3, 5];
+            myArray = new int[,]
+            {
+                {1, 45, 12, 51, 453 },
+                {112, 43, 1, 214, 123 },
+                {17, 345, 5, 34, 57 },
+            };
             myArray[0, 2] = 99;
-            Console.WriteLine(myArray[0, 2]);
+
+            foreach (var item in myArray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+
+            for (int y = 0; y < myArray.GetLength(0); y++)
+            {
+                for (int x = 0; x < myArray.GetLength(1); x++)
+                {
+                    Console.Write(myArray[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
