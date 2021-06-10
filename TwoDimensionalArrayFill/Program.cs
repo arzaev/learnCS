64f64253c8571a74d981b7ea4bea65b795bbe6e8
Ilya <arzaevilya@gmail.com>
2021-06-10 17:00:53 +0200

    stepArray

---
+++ TwoDimensionalArrayFill/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TwoDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray;
+            myArray = new int[10, 6];
+
+            Random random = new Random();
+
+            for (int y = 0; y < myArray.GetLength(0); y++)
+            {
+                for (int x = 0; x < myArray.GetLength(1); x++)
+                {
+                    myArray[y, x] = random.Next(100);
+                }
+                
+            }
+
+
+            for (int y = 0; y < myArray.GetLength(0); y++)
+            {
+                for (int x = 0; x < myArray.GetLength(1); x++)
+                {
+                    Console.Write(myArray[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
