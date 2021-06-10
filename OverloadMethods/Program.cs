e2615de073fc2c90e418281dfccc9545f1aca160
Ilya <arzaevilya@gmail.com>
2021-06-10 21:58:22 +0200

    overload

---
+++ OverloadMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OverloadMethods
+{
+    class Program
+    {
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static double Sum(double a, double b)
+        {
+            return a * b;
+        }
+
+        static void Main(string[] args)
+        {
+            int result = Sum(2, 3, 4);
+            double resultDouble = Sum(3.5, 3.4);
+            Console.WriteLine(result);
+            Console.WriteLine(resultDouble);
+        }
+    }
+}
---
+++ Visibility/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Visibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+        }
+    }
+}
