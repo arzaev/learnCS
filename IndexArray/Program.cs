1c39635415ac2c9d1e1bfcab12497d6470e5bb81
Ilya <arzaevilya@gmail.com>
2021-06-10 15:28:26 +0200

    indexArray

---
+++ IndexArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IndexArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 3, 5, 35, 2, 53 };
+            int[] myArray2 = myArray[1..4];
+            Console.WriteLine(myArray[^1]);
+        }
+    }
+}
