7735e0142fd9c143c14ac8d6a721fbcdf7c1ae27
Ilya <arzaevilya@gmail.com>
2021-06-10 20:11:59 +0200

    doneHomeWorkMethods

---
+++ HomeWorkMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HomeWorkMethods
+{
+    class Program
+    {
+
+        static string multString(string str, int mult)
+        {
+            string bar = "";
+            for (int i = 0; i < mult; i++)
+            {
+                bar += str;
+            }
+            return bar;
+        }
+
+        static int firstIndex(int[] myArray)
+        {
+            if (myArray.Length != 0)
+            {
+                return myArray[0];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("How many times do you want to print your string?: ");
+            int times = int.Parse(Console.ReadLine());
+            string result = multString(str, times);
+            Console.WriteLine(result);
+
+            int[] myArray = { 34, 46, 23, 12, 54, 3 };
+            int resultIndex = firstIndex(myArray);
+            Console.WriteLine(resultIndex);
+        }
+    }
+}
--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,9 +4,27 @@ namespace Methods
 {
     class Program
     {
+        static int Sum(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+        static void PrintResult(int result)
+        {
+            Console.WriteLine(result);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = Sum(a, b);
+            PrintResult(c);
         }
     }
 }
