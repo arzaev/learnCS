ff457136985f66eb4dc56a80cd5c9842ed8a5533
Ilya <arzaevilya@gmail.com>
2021-06-10 15:54:17 +0200

    twodemarray

---
+++ TwoDimensionalArray/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwoDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray;
+            myArray = new int[3, 5];
+            myArray[0, 2] = 99;
+            Console.WriteLine(myArray[0, 2]);
+        }
+    }
+}
