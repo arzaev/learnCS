5ce4e8091b32d252a24b3212b897768f63bddf3b
Ilya <arzaevilya@gmail.com>
2021-06-11 18:33:36 +0200

    remove element from array

--- AddingElementsInArray/Program.cs
+++ AddingElementsInArray/Program.cs
@@ -9,11 +9,34 @@ namespace AddingElementsInArray
         {
             int[] newArray = new int[array.Length + 1];
             newArray[index] = value;
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            for (int i = index; i < array.Length; i++)
+            {
+                newArray[i + 1] = array[i];
+            }
+
+            array = newArray;
+
+        }
+
+        static void AddFirst(ref int[] array, int value)
+        {
+            Insert(ref array, value, 0);
+        }
+
+        static void AddLast(ref int[] array, int value)
+        {
+            Insert(ref array, value, array.Length);
         }
 
         static void Main(string[] args)
         {
-            
+            int[] myArray = { 1, 4, 6, 2 };
+            Insert(ref myArray, -5, 2);
         }
     }
 }
---
+++ deleteElementFromArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace deleteElementFromArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static void RemoveAt(ref int[] array, int index)
+            {
+
+            }
+
+            int[] myArray = { 1, 4, 6, 2 };
+        }
+    }
+}
