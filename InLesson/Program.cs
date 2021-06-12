9e0b17ed528cfcc51397dbfe8c70625179e2a98e
Ilya <arzaevilya@gmail.com>
2021-06-12 16:12:44 +0200

    start params lesson

---
+++ InLesson/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InLesson
+{
+    class Program
+    {
+
+        static void Foo(in int value)
+        {
+            Console.WriteLine(value);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Foo(a);
+        }
+    }
+}
---
+++ OutRefLesson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OutRefLesson
+{
+    class Program
+    {
+
+        static void Foo(ref int value)
+        {
+            value++;
+            Console.WriteLine(value);
+        }
+
+        static void Bar(out int value)
+        {
+            value = 5;
+            Console.WriteLine(value);
+        }
+
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //Foo(ref a);
+            //Console.WriteLine(a);
+
+            //int a;
+            //Bar(out a);
+            //Console.WriteLine(a);
+
+            string str = Console.ReadLine();
+            int.TryParse(str, out int result);
+            Console.WriteLine(result);
+        }
+    }
+}
