3fa116e9b28fcb6d24a4c32a28b042ef760ed8c4
Ilya <arzaevilya@gmail.com>
2021-06-12 17:42:06 +0200

    start recursion

---
+++ NotNecessaryParams/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotNecessaryParams
+{
+    class Program
+    {
+
+        static int Sum(int a, int b, bool enableLogging = false)
+        {
+
+            int result = a + b;
+
+            if (enableLogging)
+            {
+                Console.WriteLine("a = " + a);
+                Console.WriteLine("b = " + b);
+                Console.WriteLine("result = " + result);
+
+            }
+
+            return result;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            //Sum(5, 43, true);
+            Sum(b: 3, a: 5, enableLogging: true);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ RecursionLesson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionLesson
+{
+    class Program
+    {
+
+        static void Foo(int i)
+        {
+            Console.WriteLine(i);
+
+            i++;
+            Foo(i);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Foo(0);
+        }
+    }
+}
