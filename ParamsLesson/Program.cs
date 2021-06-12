a1e0dd0806bf83ee4a6e29cf384f975c307a3599
Ilya <arzaevilya@gmail.com>
2021-06-12 16:55:33 +0200

    params lesson 2

--- ParamsLesson/Program.cs
+++ ParamsLesson/Program.cs
@@ -8,9 +8,35 @@ namespace ParamsLesson
 {
     class Program
     {
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static int Sum(string str, params int[] parameters)
+        {
+            int result = 0;
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                result += parameters[i];
+                Console.WriteLine(str);
+            }
+            return result;
+        }
+
+
         static void Main(string[] args)
         {
+            int result = Sum("hello", 5, 10, 3);
+            Console.WriteLine(result);
 
+            Console.ReadLine();
         }
     }
 }
---
+++ ParamsLesson2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsLesson2
+{
+    class Program
+    {
+        static void Foo(params object[] parameters)
+        {
+            string message = "Type data {0}, item {1}";
+
+            foreach (var item in parameters)
+            {
+                Console.WriteLine(message, item.GetType(), item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Foo("hello", 5, "No", 4.5, true);
+
+            Console.ReadLine();
+        }
+    }
+}
