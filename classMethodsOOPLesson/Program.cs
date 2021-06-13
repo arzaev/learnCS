cb6f83876cf835a2dfe487b222067fce11060a4b
Ilya <arzaevilya@gmail.com>
2021-06-14 00:12:02 +0200

    start topic access modifiers

---
+++ classMethodsOOPLesson/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethodsOOPLesson
+{
+    class Program
+    {
+        static Student GetStudent()
+        {
+            var student = new Student();
+
+            student.firstName = "Mac";
+            student.middleName = "Foraster";
+            student.lastName = "Dugin";
+            student.age = 19;
+            student.id = Guid.NewGuid();
+            student.group = "group1";
+
+            return student;
+        }
+
+        static void Main(string[] args)
+        {
+            var firstStudent = GetStudent();
+            firstStudent.Print();
+
+            var secondStudent = new Student();
+            secondStudent.Print();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ classMethodsOOPLesson/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethodsOOPLesson
+{
+    class Student
+    {
+        public Guid id;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public string group;
+
+        public void Print()
+        {
+            Console.WriteLine("Information about student");
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Surname: {lastName}");
+            Console.WriteLine($"Name: {firstName}");
+            Console.WriteLine($"Middle name: {middleName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Group: {group}");
+        }
+
+        public string GetFullName()
+        {
+            return $"{lastName} {firstName} {middleName}";
+        }
+    }
+}
