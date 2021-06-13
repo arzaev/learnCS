b8b53d900e92328c070f55e9816b0350c1d8eda5
Ilya <arzaevilya@gmail.com>
2021-06-14 01:56:59 +0200

    done encapsulation

--- EncapsulationOOPlesson/Program.cs
+++ EncapsulationOOPlesson/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace EncapsulationOOPlesson
 {
+
+    class Gun
+    {
+        private bool isLoaded;
+
+        private void Reload()
+        {
+            Console.WriteLine("Load...");
+
+            isLoaded = true;
+
+            Console.WriteLine("Loaded!");
+        }
+
+        public void Shoot()
+        {
+            if (!isLoaded)
+            {
+                Console.WriteLine("Gun is not loaded");
+                Reload();
+            }
+
+            Console.WriteLine("Shot - shot\n");
+            isLoaded = false;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Gun gun = new Gun();
+            gun.Shoot();
+            Console.ReadLine();
         }
     }
 }
