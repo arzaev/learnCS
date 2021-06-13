3689ee8ee1cab3e3a317d623ee0967ac72754816
Ilya <arzaevilya@gmail.com>
2021-06-14 01:25:39 +0200

    start encapsulation

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace AccessModifiers
 {
+
+    class Point
+    {
+        int z = 3; // <--default private
+        public int x = 3;
+        private int y = 5;
+
+        private void PrintX()
+        {
+            Console.WriteLine($"X {x}");
+        }
+
+        public void PrintY()
+        {
+            Console.WriteLine($"Y {y}");
+        }
+
+        public void PrintPoint()
+        {
+            PrintX();
+            PrintY();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Point point = new Point();
+            point.PrintPoint();
+
+            Console.ReadLine();
         }
     }
 }
