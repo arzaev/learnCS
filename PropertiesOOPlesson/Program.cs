419706669ca5c5c351756b9ae54d485df2169c7b
Ilya <arzaevilya@gmail.com>
2021-06-14 15:16:02 +0200

    get set

--- PropertiesOOPlesson/Program.cs
+++ PropertiesOOPlesson/Program.cs
@@ -4,12 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// propfull
+
 namespace PropertiesOOPlesson
 {
+
+    class Point
+    {
+        private int x;
+
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+
+        private int y;
+
+        public int Y
+        {
+            get 
+            { 
+                return y; 
+            }
+            set 
+            { 
+                y = value; 
+            }
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Point point = new Point();
+
+            point.Y = 10;
+
+            int y = point.Y;
+
+
         }
     }
 }
