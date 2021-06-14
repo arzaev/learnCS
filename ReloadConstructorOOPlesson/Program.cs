f159043a9f6978374c8f602d0851e7c8165f7fb9
Ilya <arzaevilya@gmail.com>
2021-06-14 12:37:12 +0200

    reload constructor lesson

---
+++ ReloadConstructorOOPlesson/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReloadConstructorOOPlesson
+{
+
+    class Point
+    {
+
+        public Point()
+        {
+            _x = _y = 1;
+        }
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        private int _x;
+        private int _y;
+
+        public void Print()
+        {
+            Console.WriteLine($"X: {_x}\tY: {_y}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.Print();
+        }
+    }
+}
