093ff971c928b5023c2eb38b92a0ebfc6ddb3998
Ilya <arzaevilya@gmail.com>
2021-06-16 14:53:02 +0200

    const readonly

---
+++ constReadOnly/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constReadOnly
+{
+    class Program
+    {
+
+        class MyClass
+        {
+            public const double PI = 3.14;
+
+            public const string MY_ERROR = "some error";
+
+            public readonly int _a;
+
+            public static readonly int _b;
+
+            public MyClass(int a)
+            {
+                _a = a;
+            }
+
+            public void Foo()
+            {
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass myclass = new MyClass(4);
+            myclass.Foo();
+        }
+    }
+}
