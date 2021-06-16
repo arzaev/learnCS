b1dec7ec1b76ed6b055e1d1b5c23f16d6b0fe7ed
Ilya <arzaevilya@gmail.com>
2021-06-16 16:37:28 +0200

    inheritance

---
+++ InheritanceOOPlesson/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceOOPlesson
+{
+
+    class Person
+    {
+        public int a;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void PrintName()
+        {
+            Console.WriteLine($"My name is {FirstName}!");
+        }
+    }
+
+    class Student : Person
+    {
+        public void Learn()
+        {
+            Console.WriteLine("I am learning!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student() { FirstName = "Mike", LastName = "Fox" };
+            student.PrintName();
+            student.Learn();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InitObjects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitObjects
+{
+    class Cat
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Cat()
+        {
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat();
+            cat1.Age = 3;
+            cat1.Name = "Tom";
+
+            Cat cat2 = new Cat
+            {
+                Age = 4,
+                Name = "Tom"
+            };
+        }
+    }
+}
