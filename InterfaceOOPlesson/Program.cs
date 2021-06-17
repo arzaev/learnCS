1a5797744db7ac3035892e8255bdae54ff2eca18
Ilya <arzaevilya@gmail.com>
2021-06-17 21:49:06 +0200

    interface

---
+++ InterfaceOOPlesson/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceOOPlesson
+{
+    interface IDataProvider
+    {
+        string GetData();
+    }
+
+    interface IDataProcessor
+    {
+        void ProcessData(IDataProvider dataProvider);
+    }
+
+    class ConsoleDataProcessor : IDataProcessor
+    {
+        public void ProcessData(IDataProvider dataProvider)
+        {
+            Console.WriteLine(dataProvider.GetData());
+        }
+    }
+
+    class DbDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Data from DATA BASE";
+        }
+    }
+
+    class FileDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Data from File";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataProcessor dataProcessor = new ConsoleDataProcessor();
+            dataProcessor.ProcessData(new DbDataProvider());
+            dataProcessor.ProcessData(new FileDataProvider());
+
+            Console.ReadLine();
+        }
+    }
+}
