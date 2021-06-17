becc6e4fbb63248edb74a45d3d09bef648867584
Ilya <arzaevilya@gmail.com>
2021-06-17 17:25:25 +0200

    polymorphism

---
+++ PolymorphismOOP/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// virtual
+
+// override
+
+
+namespace PolymorphismOOP
+{
+
+    class Car
+    {
+        public virtual void Drive()
+        {
+            Console.WriteLine("I am car, I am driving");
+        }
+    }
+
+    class SportCar : Car
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("I am driving very fast");
+        }
+    }
+
+    class Person
+    {
+        public void Drive(Car car)
+        {
+            car.Drive();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Person person = new Person();
+            person.Drive(new SportCar());
+            Console.ReadLine();
+        }
+    }
+}
