0954823332d7bc33185fa2fa198aa82c99224c1f
Ilya <arzaevilya@gmail.com>
2021-06-17 16:19:26 +0200

    protected

---
+++ inheritanceAccessModifiers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritanceAccessModifiers
+{
+    class A
+    {
+        public int publicField;
+        private int privateField;
+        protected int protectedField;
+
+        public A()
+        {
+            Console.WriteLine(publicField);
+            Console.WriteLine(privateField);
+            Console.WriteLine(protectedField);
+        }
+    }
+
+    class B : A
+    {
+        public B()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
